6770bf451b240e89e4d69f0f2d94965fe24e51ec
JavohirBozorboyev <javohirbozorboyev2002@gmail.com>
2022-06-18 15:31:22 +0500

    new style

--- js/index.js
+++ js/index.js
@@ -1139,3 +1139,4 @@ modalSearch.addEventListener("keyup", (e) => {
   });
 });
 
+
--- js/modal.js
+++ js/modal.js
@@ -1,35 +1,3 @@
-let selName = document.querySelectorAll(".selName");
-let modalSearch = document.querySelector(".modalSearch");
-let modQosh = document.querySelector(".modQosh");
-let modQid = document.querySelector(".modQid");
-
-
-
-
-
-
-// modalSearch.addEventListener("keyup", (e) => {
-//   let value = e.target.value;
-//   if (value == "") {
-//     for (let i = 0; i < selName.length; i++) {
-//       selName[i].style.display = "block";
-//     }
-//   }
-//   selName.forEach((item) => {
-//     if (item.innerHTML.toLowerCase().includes(value.toLowerCase())) {
-//       item.style.display = "block";
-//     } else {
-//       item.style.display = "none";
-//     }
-//   });
-// });
-
-
-
-
-
-
-
 
 
 $(document).ready(function() {
